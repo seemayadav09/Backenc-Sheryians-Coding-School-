6e2b426b8e61d67b09a8e98d34251a7333ab49c3
Seema Yadav <seemayadav982002@gmail.com>
2023-11-14 22:14:59 +0530

    first commit

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-// var figlet=require(`figlet`);
 
-// figlet("Happy Diwali",function (err, data) {
-//     if (err) {
-//       console.log("Something went wrong...");
-//       console.dir(err);
-//       return;
-//     }
-//     console.log(data); 
-//   });
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000 
 
-app.use(function(req,res,next){
-    console.log("hello from middleware");
-    next();
-});
+// app.use(function(req,res,next){
+//     console.log("hello from middleware");
+//     next();
+// });
 //there is an issue with middleware, agr middleware chal gya toh request jam ho jati aur route tk nhi phuch paati
 //that is why we use next()
-
+app.set("view engine","ejs");
+app.use(express.static('./public'));
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  // res.send('Hello World!')
+  res.render("index",{age:12}) //index page pr jha jha age hoga wha wha 12 aa jayega
+
+
+})
+
+//make sure ki render krte wqt views folder ki hi kisi file ka naam likhe
+app.get("/error",function(req, res,next){
+  throw Error("something went wrong")
+})
+app.use(function errorHandler (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.render('error', { error: err })
 })
 
 app.listen(port, () => {
